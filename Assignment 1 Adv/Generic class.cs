d76d76f4b59d3f86733284633a195cf6f79ca004
DiabloXV <haboali1001@gmail.com>
2024-07-26 02:11:44 +0300

    Questions 1,2 solved

---
+++ Assignment 1 Adv/Generic class.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_1_Adv
+{
+    public class Range<T> where T : IComparable<T>
+    {
+        public T Minimum { get; private set; }
+        public T Maximum { get; private set; }
+
+        public Range(T minimum, T maximum)
+        {
+            if (minimum.CompareTo(maximum) > 0)
+            {
+                throw new ArgumentException("Minimum value should be less than or equal to the maximum value.");
+            }
+
+            Minimum = minimum;
+            Maximum = maximum;
+        }
+
+        public bool IsInRange(T value)
+        {
+            return value.CompareTo(Minimum) >= 0 && value.CompareTo(Maximum) <= 0;
+        }
+
+        public T Length()
+        {
+            dynamic min = Minimum;
+            dynamic max = Maximum;
+            return max - min;
+        }
+    }
+
+    
+    
+}
